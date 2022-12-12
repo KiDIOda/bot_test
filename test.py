e50404b60c68cf123e1f0f04168b196283ea899c
KiDIOda <jimasdfghjkl123456789@gmail.com>
2022-12-12 15:53:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("111")
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='[')
+
+@bot.event
+async def on_ready():
+    print(">>A<<")
+
+bot.run('MTA0OTY2NzYwNzYyOTk0Mjg0NA.GhqyCE.tC1CZvoOwdfFO5H2w12GKFuPC5-96X4cwLpSpQ')
