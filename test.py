75f85519522c2d54d54014e32d69d427c569a669
KiDIOda <jimasdfghjkl123456789@gmail.com>
2022-12-12 16:04:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='[')
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
 @bot.event
 async def on_ready():
     print(">>A<<")
 
-bot.run('MTA0OTY2NzYwNzYyOTk0Mjg0NA.GhqyCE.tC1CZvoOwdfFO5H2w12GKFuPC5-96X4cwLpSpQ')
+bot.run('')
